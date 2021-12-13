93cc1936c2b855a3acc961c5be846ff45819d36b
João Henrique <joaohenriquetdj@gmail.com>
2021-12-13 14:08:25 -0300

    add init refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-import { parseCookies } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333/",
@@ -10,3 +10,37 @@ export const api = axios.create({
     Authorization: `Bearer ${cookies["@app.token"]}`,
   },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+        const { "@app.refreshToken": refreshToken } = cookies;
+
+        api.post("refresh", { refreshToken }).then((response) => {
+          const token = response.data.token;
+
+          setCookie(undefined, "@app.token", token, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: "/",
+          });
+          setCookie(
+            undefined,
+            "@app.refreshToken",
+            response.data.refreshToken,
+            {
+              maxAge: 60 * 60 * 24 * 30,
+              path: "/",
+            }
+          );
+          api.defaults.headers["Authorization"] = `Bearer ${token}`;
+        });
+      } else {
+      }
+    }
+  }
+);
